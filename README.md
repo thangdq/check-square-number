f9b5df7b02268c34f1f575846b55bdfd6741287f
ThangDQ <thangdq@greenglobal.vn>
2016-10-06 08:33:33 +0700

    feat(Coveralls): Update coveralls

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Check number is square number
 
 [![Build Status](https://travis-ci.org/thangdq/check-square-number.svg?branch=master)](https://travis-ci.org/thangdq/check-square-number)
-[![Coverage Status](https://coveralls.io/repos/github/thangdq/check-square-number/badge.svg?branch=master)](https://coveralls.io/github/thangdq/check-square-number?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/thangdq/check-square-number/badge.svg)](https://coveralls.io/github/thangdq/check-square-number)
 
 ## Installation
 
--- test/index.js
+++ test/index.js
@@ -1,45 +1,45 @@
 var expect = require('chai').expect;
-var check_numbers = require('../src/index.js');
+var checkNumbers = require('../src/index.js');
 
-describe('SquareNumbers', function(){
-	describe('checkSquareNumber()', function(){
-		it('return error when number is not number', function(done){
-			check_numbers.checkSquareNumber('abc', function(err, result){				
-				expect(err.message).to.equal('is not number');
-				done();
-			});
-		});
-		
-		it('return false when number is 0', function(done){
-			check_numbers.checkSquareNumber(0, function(err, result){
-				expect(err).to.equal(null);
-				expect(result).to.equal(false);
-				done();
-			});
-		});
-		
-		it('return false when number is 15', function(done){
-			check_numbers.checkSquareNumber(15, function(err, result){
-				expect(err).to.equal(null);
-				expect(result).to.equal(false);
-				done();
-			});
-		});
-		
-		it('return false when number is -16', function(done){
-			check_numbers.checkSquareNumber(-16, function(err, result){
-				expect(err).to.equal(null);
-				expect(result).to.equal(false);
-				done();
-			});
-		});
-		
-		it('return true when number is 16', function(done){
-			check_numbers.checkSquareNumber(16, function(err, result){
-				expect(err).to.equal(null);
-				expect(result).to.equal(true);
-				done();
-			});
-		});		
-	});
+describe('SquareNumbers', () => {
+  describe('checkSquareNumber()', () => {
+    it('return error when number is not number', (done) => {
+      checkNumbers.checkSquareNumber('abc', (err) => {
+        expect(err.message).to.equal('is not number');
+        done();
+      });
+    });
+
+    it('return false when number is 0', (done) => {
+      checkNumbers.checkSquareNumber(0, (err, result) => {
+        expect(err).to.equal(null);
+        expect(result).to.equal(false);
+        done();
+      });
+    });
+
+    it('return false when number is 15', (done) => {
+      checkNumbers.checkSquareNumber(15, (err, result) => {
+        expect(err).to.equal(null);
+        expect(result).to.equal(false);
+        done();
+      });
+    });
+
+    it('return false when number is -16', (done) => {
+      checkNumbers.checkSquareNumber(-16, (err, result) => {
+        expect(err).to.equal(null);
+        expect(result).to.equal(false);
+        done();
+      });
+    });
+
+    it('return true when number is 16', (done) => {
+      checkNumbers.checkSquareNumber(16, (err, result) => {
+        expect(err).to.equal(null);
+        expect(result).to.equal(true);
+        done();
+      });
+    });
+  });
 });
